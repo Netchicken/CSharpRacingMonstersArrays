47cd0012d7ca1ddbf1a4a38716e6af8425b397d3
Gary Dix <intelproof@gmail.com>
2024-07-15 16:35:36 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace C_RacingMonsters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRace = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRace
+            // 
+            this.btnRace.Location = new System.Drawing.Point(471, 13);
+            this.btnRace.Name = "btnRace";
+            this.btnRace.Size = new System.Drawing.Size(101, 50);
+            this.btnRace.TabIndex = 4;
+            this.btnRace.Text = "Race!";
+            this.btnRace.UseVisualStyleBackColor = true;
+            this.btnRace.Click += new System.EventHandler(this.btnRace_Click);
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblWinner.Location = new System.Drawing.Point(68, 12);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(0, 24);
+            this.lblWinner.TabIndex = 5;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(589, 13);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 50);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::C_RacingMonsters.Properties.Resources.bg_jpg;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::C_RacingMonsters.Properties.Resources.bg_jpg;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(719, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(736, 395);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.btnRace);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRace;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_RacingMonsters
+{
+    public partial class Form1 : Form
+    {
+        Random myRandom = new Random();
+        int gameStart = 44;
+        //create an array of pictureboxes
+        PictureBox[] pbx = new PictureBox[4];
+
+        int[] yAlign = new int[] { 65, 140, 217, 284 };
+
+
+        public Form1()
+        {
+
+
+            for (int i = 0; i < pbx.Length; i++)
+            {
+                pbx[i] = new PictureBox();
+                pbx[i].Name = "pbBox" + i;
+                pbx[i].Size = new Size(70, 77);
+                pbx[i].BorderStyle = BorderStyle.FixedSingle;
+                pbx[i].Location = new Point(42, yAlign[i]);
+                pbx[i].Visible = true;
+                pbx[i].SizeMode = PictureBoxSizeMode.StretchImage;
+
+                this.Controls.Add(pbx[i]);
+            }
+            pbx[0].Image = Resource1.Ugor;
+            pbx[1].Image = Resource1.Agor;
+            pbx[2].Image = Resource1.Igor;
+            pbx[3].Image = Resource1.Ogor;
+
+            pbx[0].Tag = "Ugor";
+            pbx[1].Tag = "Agor";
+            pbx[2].Tag = "Igor";
+            pbx[3].Tag = "Ogor";
+
+
+
+
+            InitializeComponent();
+        }
+
+        private void btnRace_Click(object sender, EventArgs e)
+        {
+            var gameEnd = 624;
+
+            bool isEnd = false;
+            string winner = "";
+
+            while (!isEnd)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var ran = myRandom.Next(1, 50);
+                    Thread.Sleep(50);
+                    pbx[i].Location = new Point(pbx[i].Location.X + ran, pbx[i].Location.Y);
+
+                    if (pbx[i].Location.X >= gameEnd)
+                    {
+                        isEnd = true;
+                        winner = pbx[i].Tag.ToString();
+                        lblWinner.Text = winner + " is the WINNER";
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                pbx[i].Location = new Point(gameStart, pbx[i].Location.Y);
+            }
+
+
+        }
+    }
+}
